9bb1f8732a24a6268bb84b58e82d5b597e014113
giorgiotopa <145937688+giorgiotopa@users.noreply.github.com>
2024-03-09 18:29:58 +0100

    Modifiche Security Chain, aggiornamento cors

--- src/main/java/it/epicode/GestioneAuto/security/SecurityChain.java
+++ src/main/java/it/epicode/GestioneAuto/security/SecurityChain.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +15,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.*;
 
@@ -28,7 +31,8 @@ public class SecurityChain {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
-        httpSecurity.cors(AbstractHttpConfigurer::disable);
+//        httpSecurity.cors(AbstractHttpConfigurer::disable);
+        httpSecurity.cors(Customizer.withDefaults());
 
         httpSecurity.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
@@ -43,8 +47,9 @@ public class SecurityChain {
     @Bean
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration cors = new CorsConfiguration();
-        cors.addAllowedOrigin("http://www.example.com");
-        cors.addAllowedMethod(HttpMethod.GET);
+        cors.setAllowedOrigins(List.of("http://localhost:**"));
+        cors.setAllowedMethods(List.of("GET"));
+        cors.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
         configurationSource.registerCorsConfiguration("*/**", cors);
         return configurationSource;
